f2c081a92e4f4dfa36addda0379d36f77c207992
Stefan Ritt <stefan.ritt@psi.ch>
2002-11-25 08:01:27 +0000

    Added make of release
    SVN revision: 293

--- build.bat
+++ build.bat
@@ -4,6 +4,12 @@ rem Build ELOG distribution
 
 set version=%1
 
+cd nt
+nmake -f elog.mak CFG="elog - Win32 Release"
+nmake -f elogd.mak CFG="elogd - Win32 Release"
+nmake -f elconv.mak CFG="elconv - Win32 Release"
+cd ..
+
 rem map network drive
 net use m: /d > nul
 net use m: \\pc2075\midas mi_das /user:midas
