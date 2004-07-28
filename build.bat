88d9d61626c22bd9c4e481f0d479df8a31e29b07
Stefan Ritt <stefan.ritt@psi.ch>
2004-07-28 11:22:28 +0000

    *** empty log message ***
    SVN revision: 986

--- build.bat
+++ build.bat
@@ -10,12 +10,8 @@ nmake -f elogd.mak CFG="elogd - Win32 Release"
 nmake -f elconv.mak CFG="elconv - Win32 Release"
 cd ..
 
-rem map network drive
-net use n: /d > nul
-net use n: \\pc2075\midas mi_das /user:midas
-
 "\program files\NSIS\makensis" /DVERSION=%version% elog.nsi
 
-copy elog%version%.exe n:\html\elog\download\windows\
-copy elog%version%.exe n:\html\elog\download\windows\elog-latest.exe
+scp elog%version%.exe midas@midas:html/elog/download/windows/
+scp elog%version%.exe midas@midas:html/elog/download/windows/elog-latest.exe
 
--- buildsnap.bat
+++ buildsnap.bat
@@ -3,9 +3,5 @@ cd nt
 nmake -f elogd.mak CFG="elogd - Win32 Release"
 cd ..
 
-rem map network drive
-net use n: /d > nul
-net use n: \\pc2075\midas mi_das /user:midas
-
-copy nt\Release\elogd.exe n:\html\elog\download\windows\elogd-snapshot.exe
+scp nt\Release\elogd.exe midas@midas:html/elog/download/windows/elogd-snapshot.exe
 
