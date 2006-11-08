4a6fae75285981449ef9d210cb8e934d09e62d99
Stefan Ritt <stefan.ritt@psi.ch>
2006-11-08 15:52:18 +0000

    Prevent crash if logbook "global<xxx>" is accessed and a logbook "global<xxx>" is defined in config file
    SVN revision: 1748

--- src/elogd.c
+++ src/elogd.c
@@ -2645,9 +2645,13 @@ int is_logbook(char *logbook)
 {
    char str[256];
 
+   if (strieq(logbook, "global"))
+      return 0;
+
+   /* check for 'global group' or 'global_xxx' */
    strlcpy(str, logbook, sizeof(str));
-   str[6] = 0;
-   return !strieq(str, "global");
+   str[7] = 0;
+   return !strieq(str, "global ");
 }
 
 /*-------------------------------------------------------------------*/
