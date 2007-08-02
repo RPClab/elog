3b6629d2c8256ce2ed9e158064d84a035509b2c7
Stefan Ritt <stefan.ritt@psi.ch>
2007-08-02 16:08:28 +0000

    Merged toolbars
    SVN revision: 1900

--- scripts/fckeditor/fckelog.js
+++ scripts/fckeditor/fckelog.js
@@ -6,7 +6,6 @@ FCKConfig.ToolbarSets["Default"] = [
 	['Source','-','Save','Preview'],
 	['Cut','Copy','Paste','PasteText','PasteWord','-','Print','SpellCheck'],
 	['Undo','Redo','-','Find','Replace','-','SelectAll','RemoveFormat'],
-	'/',
 	['Bold','Italic','Underline','StrikeThrough','-','Subscript','Superscript'],
 	['OrderedList','UnorderedList','-','Outdent','Indent'],
 	['JustifyLeft','JustifyCenter','JustifyRight','JustifyFull'],
