8c8806077b418767ae4805a5c34e20b2bbde9acd
Stefan Ritt <stefan.ritt@psi.ch>
2013-05-03 11:31:42 +0000

    Fixed email attachments with "thumbnail size = 0"
    SVN revision: 2489

--- src/elogd.c
+++ src/elogd.c
@@ -21313,9 +21313,10 @@ void format_email_attachments(LOGBOOK * lbs, int message_id, int attachment_type
       /* encode file */
       strlcpy(file_name, lbs->data_dir, sizeof(file_name));
       strlcat(file_name, att_file[index], sizeof(file_name));
-      if (is_image(file_name) && image_magick_exist) {
+      if (is_image(file_name)) {
          get_thumb_name(file_name, str, sizeof(str), 0);
-         strlcpy(file_name, str, sizeof(file_name));
+         if (str[0])
+            strlcpy(file_name, str, sizeof(file_name));
       }
 
       fh = open(file_name, O_RDONLY | O_BINARY);
