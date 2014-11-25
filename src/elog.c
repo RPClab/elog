ed4d4f9eae7cc20d65929ed0c82f7cd3e981a10e
Stefan Ritt <stefan.ritt@psi.ch>
2014-11-25 15:19:01 +0100

    Replaced SSLv23 method by TLSv1 method

--- src/elog.c
+++ src/elog.c
@@ -292,7 +292,7 @@ int ssl_connect(int sock, SSL ** ssl_con)
    SSL_library_init();
    SSL_load_error_strings();
 
-   meth = (SSL_METHOD *) SSLv23_method();
+   meth = (SSL_METHOD *) TLSv1_method();
    ctx = SSL_CTX_new(meth);
 
    *ssl_con = SSL_new(ctx);
